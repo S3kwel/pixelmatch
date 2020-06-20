b83b3fcfb0673e5cf8294dc0139ec79201c9f023
Dustin Hickman <43394966+S3kwel@users.noreply.github.com>
2020-06-20 01:29:21 -0400

    Coordinate Conversion Complete

--- app.js
+++ app.js
@@ -3,101 +3,162 @@ const PNG = require('pngjs').PNG;
 const fs = require('fs');
 const [, , imgPath, outputPath] = process.argv;
 
+/**
+ * NOTES 
+ *         
+//CONDITONS:
+//A: A pixel has transparency to the left of a solid edge at +1x
+//B: A pixel has transparency to the right of a solid edge at -1x
+//C: A pixel has transparency to the left and the right of separate solid edges.
+
+//A: Place a vertex at +1x.
+//B: Place a vertex at -1X.
+//C: Place a vertex at +1X AND -1X.
+
+Float values are irrelevant in the sense that we can get the general shape of the image better without snapping to pixels.  
+When examining the pixels themselves, this approach will force us to have to apply a 'brush' of pixels along the vector lines. 
 
 
-var options = { transparency: 1.0 }; 
+ * 
+ * */
 
 
+class ImgGrid {
+    constructor(img, outputImg = false) {
+        this.img = img != null && this.isPixelData(img.data) ? img.data : null;
+        this.outPutImage = outputImg ? new PNG({ w, h }).data : null;
 
+        this.imgWidth = this.img ? img.width : 0;
+        this.imgHeight = this.img ? img.height : 0;
+        this.points = this.generatePoints();
 
-function getXFor(x,width) {
-    return (x - (width / 2)); 
-}
-
-function getYFor(y, height) {
-    let newY =  y - (height / 2)
-
-    if (Math.sign(newY) == -1) {
-        return Math.abs(newY);
+        //console.log("CENTER POINT IS: "); 
+        this.getPoint(0, 0)
+        //console.log(this.getPoint(0, 0)); 
+        this.getPixelAt(0, 0); 
     }
 
-    else {
-        return -Math.abs(newY); 
+
+    isPixelData(arr) {
+        // work around instanceof Uint8Array not working properly in some Jest environments
+        return ArrayBuffer.isView(arr) && arr.constructor.BYTES_PER_ELEMENT === 1;
     }
-}
 
-//Intended for pixel position/4 in a top-down, left-right config.  
-function getCoordsFor(x,y, width, height){
-    let xCord = getXFor(x, width); 
-    let yCord = getXFor(y, height); 
+    //Points start from 0, top down, left right.  
+    generatePoints() {
+        let returnArray = []; 
+        
+        
 
-    return { x: xCord, y: yCord }; 
-}
+        for (let x = 0; x < this.imgWidth; x++) {
+            for (let y = 0; y < this.imgHeight; y++) {
+               if (y == this.imgHeight-1 && typeof this.maxY == 'undefined') { 
+                   this.maxY = y; 
+               }
 
+            }
+            if (x == this.imgWidth-1 && typeof this.maxX == 'undefined') {
+                this.maxX = x; 
+            }
+        }
 
+         for (let x = 0; x < this.imgWidth; x++) {
+            for (let y = 0; y < this.imgHeight; y++) {
+                let point = new Point(x, y, this.imgWidth, this.imgHeight, this.maxX, this.maxY);
+                returnArray.push(point); 
+            }
+        }
+         
+         
+        return returnArray; 
+    }
 
-
-function getTransparency(img, output, width, height) {
-
-    if (!isPixelData(img) || (output && !isPixelData(output)))
-        throw new Error('Image data: Uint8Array, Uint8ClampedArray or Buffer expected.');
-
-    let workingOutput = output;
-
-    if(!output)
-        workingOutput = img;
-
-    var test = []; 
-   
-
-    //Number of pixel must be multiplied by 4 to hit the right byte
-    //Pixel 0 is pixel 1
-
-  
-
-    //Go through the pixels and detect 
-    for (let x = 0; x < width; x++) {
-        for (let y = 0; y < height; y++) {
-            //Coresponds to the byte number in the array...
-            const pos = (y * width + x) * 4;
-
-          
-            console.log(getCoordsFor(x,y, width, height)); 
-
-            
-            //workingOutput = drawPixel(workingOutput, pos2, { r: 234, g: 255, b: 0 });
-          
-           
-     
-
-            let c = color(img, pos);
-            if (c.a > 220) {
-                if (!output)
-                        workingOutput = drawPixel(workingOutput, pos, c);
-                }
-            else {
-                if (!output)
-                        workingOutput = drawPixel(workingOutput, pos, {r: 255, g:0, b:21});
-                }
-          
-            
+    getPoint(x, y) {
+        for (let point of this.points) {
+            if (point.x == x && point.y == y)
+                return point; 
         }
     }
-    test.sort(function (a, b) {
-        return a - b;
-    }); 
-    console.log(test); 
-    
-    if (!output)  
-        return { out: workingOutput, data: false }; 
-    return { data: false }; 
+
+    getPixelAt(x,y) {
+       // console.log(x, y); 
+        let point = new Point(x, y, this.imgWidth, this.imgHeight, this.maxX, this.maxY); 
+
+        //console.log(point.x, point.y);
+        //let baseHeight = 1 * this.imgWidth; 
+
+
+       // console.log((point._x * (this.imgWidth * (x+1))) - (maxX-x)   ); 
+    }
+
+       
+
+   
 
 }
 
-function isPixelData(arr) {
-    // work around instanceof Uint8Array not working properly in some Jest environments
-    return ArrayBuffer.isView(arr) && arr.constructor.BYTES_PER_ELEMENT === 1;
+
+/***
+ * Point
+ * Constructor expects either an explcit value for X and Y, or a POS object of the following structure:
+ * {x:int,y:int}
+ * POS refers to the pixel position of the image in a top-down, left-right order.  
+ * each point in POS is (pixel position/4).
+ * 
+ * If POS is provided, this.x and this.y refer to coordinates in the grid rather than the original pixel-order.  
+ * */
+class Point{
+    constructor(x, y, width, height,maxX,maxY) {  
+        this._width = width;
+        this._height = height;
+        this.maxX = maxX;
+        this.maxY = maxY; 
+
+
+        let oldTop = 0;
+        let oldLeft = 0;
+        let oldRight = this.maxX; 
+        let oldBottom = this.maxY;
+
+        //New left and right get pushed over by half of the old with.  
+        let newLeft = oldLeft + this.maxX / 2;
+        let newRight = oldRight + this.maxX / 2;
+        let newTop = oldTop + this.maxY / 2;  //New top should still be zero.
+        let newBottom = oldBottom; //New Bottom should still be MaxY.
+
+        //console.log(newLeft,newRight,newTop,newBottom); 
+
+        this.x = newLeft + ((x - oldLeft) / (oldRight - oldLeft)) * (newRight - newLeft);  
+        this.y = newTop + ((y - oldTop) / (oldBottom - oldTop)) * (newBottom - newTop); 
+   
+        console.log(this.x)
+        console.log(this.y);
+
+        process.exit(); 
+
+
+
+        this._x = x;    
+        this._y = y;
+    }
+  
+    //NOTE:  Will need to inherit img from grid.  
+    //NOTE:  Will need to inherit witdth and height from grid.  
+    color() {  
+        let r = this.img[this.pos + 0];
+        let g = this.img[this.pos + 1];
+        let b = this.img[this.pos + 2];
+        let a = this.img[this.pos + 3];
+        return ({
+            'r': r, 'g': g, 'b': b, 'a': a
+        });
+    }
 }
+
+
+
+
+
 
 // check if a pixel has 3+ adjacent pixels of the same color.
 function hasManySiblings(img, x1, y1, width, height) {
@@ -126,16 +187,6 @@ function hasManySiblings(img, x1, y1, width, height) {
     return false;
 }
 
-function color(img, k) {
-    let r = img[k + 0];
-    let g = img[k + 1];
-    let b = img[k + 2];
-    let a = img[k + 3];
-    return ({
-        'r': r, 'g': g, 'b': b, 'a': a
-    });
-}
-
 
 function drawPixel(output, pos, c) {
     output[pos + 0] = c.r;
@@ -148,14 +199,13 @@ function drawPixel(output, pos, c) {
 
 //Read the image, get its width and height.  
 const img = PNG.sync.read(fs.readFileSync(imgPath));
-let w = img.width; 
-let h = img.height; 
 
-const outputImage = outputPath ? new PNG({ w, h }) : null;
+let g = new ImgGrid(img); 
+//let p = new Point(null,4);
 
 //Get the transparency arrays and prep the output image, if the user asked for it.  
 
-
+/*
 if (outputImage) {
     let output = getTransparency(img.data, outputImage.data, img.width, img.height);
 
@@ -171,6 +221,6 @@ else {
     let output = getTransparency(img.data, false, img.width, img.height);
     console.log(output); 
 }
-
+*/
 
 
